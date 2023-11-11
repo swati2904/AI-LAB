a8c2c0079be6089849ed38506263842e25195fb9
swati2904 <swati.lpu17@gmail.com>
2023-11-10 23:31:08 -0800

    implemented tie-breaking rule for algorithms

--- search_algorithm.py
+++ search_algorithm.py
@@ -4,15 +4,18 @@ from collections import deque
 
 class State:
     def __init__(self, name, g_value, h_value):
+        # Initialize the State with a name, g_value (cost from start), and h_value (heuristic value)
         self.name = name
         self.g_value = g_value
         self.h_value = h_value
 
     def f_value(self):
+        # Calculate the total cost (f_value) by adding g_value and h_value
         return self.g_value + self.h_value
 
     def __lt__(self, other):
-        return self.f_value() < other.f_value()
+        # Define less-than comparison based on f_value and name
+        return (self.f_value(), self.name) < (other.f_value(), other.name)
 
 
 def bfs_search(start, goal, live_traffic, sunday_traffic):
@@ -26,6 +29,7 @@ def bfs_search(start, goal, live_traffic, sunday_traffic):
         current = open_queue.popleft()
 
         if current.name == goal:
+            # Reconstruct and return the path if the goal is reached
             path = []
             while current.name in came_from:
                 path.append((current.name, current.g_value))
@@ -36,10 +40,11 @@ def bfs_search(start, goal, live_traffic, sunday_traffic):
 
         visited.add(current.name)
 
-        for neighbor, cost in live_traffic.get(current.name, []):
+        for neighbor, cost in sorted(live_traffic.get(current.name, [])):
             if neighbor not in visited and not any(
                 state.name == neighbor for state in open_queue
             ):
+                # If neighbor is not visited and not in the open_queue, add it to the queue
                 g_value = current.g_value + cost
                 h_value = sunday_traffic[neighbor]
                 neighbor_state = State(neighbor, g_value, h_value)
@@ -47,6 +52,51 @@ def bfs_search(start, goal, live_traffic, sunday_traffic):
                 came_from[neighbor_state.name] = current
 
     return None
+
+
+def ucs_search(start, goal, live_traffic, sunday_traffic):
+    # Implementation of UCS search algorithm
+
+    open_queue = [State(start, 0, sunday_traffic[start])]
+    closed_set = set()
+    came_from = {}
+
+    while open_queue:
+        current = heapq.heappop(open_queue)
+
+        if current.name == goal:
+            path = []
+            # Reconstruct and return the path if the goal is reached
+            while current.name in came_from:
+                path.append((current.name, current.g_value))
+                current = came_from[current.name]
+            path.append((start, 0))
+            path.reverse()
+            return path
+
+        closed_set.add(current.name)
+
+        for neighbor, cost in sorted(live_traffic.get(current.name, [])):
+            if neighbor in closed_set:
+                continue
+
+            g_value = current.g_value + cost
+            h_value = sunday_traffic[neighbor]
+            neighbor_state = State(neighbor, g_value, h_value)
+
+            if any(
+                state.name == neighbor and state.g_value <= g_value
+                for state in open_queue
+            ):
+                continue
+
+            heapq.heappush(open_queue, neighbor_state)
+            came_from[neighbor] = current
+
+    return None
+
+
+from collections import deque
 
 
 def dfs_search(start, goal, live_traffic, sunday_traffic):
@@ -60,6 +110,7 @@ def dfs_search(start, goal, live_traffic, sunday_traffic):
         current = open_stack.pop()
 
         if current.name == goal:
+            # If neighbor is not visited and not in the open_queue, add it to the queue
             path = []
             while current.name in came_from:
                 path.append((current.name, current.g_value))
@@ -87,44 +138,6 @@ def dfs_search(start, goal, live_traffic, sunday_traffic):
     return None
 
 
-def ucs_search(start, goal, live_traffic, sunday_traffic):
-    # Implementation of UCS search algorithm
-
-    open_queue = [State(start, 0, sunday_traffic[start])]
-    closed_set = set()
-    came_from = {}
-
-    while open_queue:
-        current = heapq.heappop(open_queue)
-
-        if current.name == goal:
-            path = []
-            while current.name in came_from:
-                path.append((current.name, current.g_value))
-                current = came_from[current.name]
-            path.append((start, 0))
-            path.reverse()
-            return path
-
-        closed_set.add(current.name)
-
-        for neighbor, cost in live_traffic.get(current.name, []):
-            if neighbor in closed_set:
-                continue
-
-            g_value = current.g_value + cost
-            h_value = sunday_traffic[neighbor]
-            neighbor_state = State(neighbor, g_value, h_value)
-
-            if neighbor_state in open_queue:
-                continue
-
-            heapq.heappush(open_queue, neighbor_state)
-            came_from[neighbor] = current
-
-    return None
-
-
 def astar_search(start, goal, live_traffic, sunday_traffic):
     # Implementation of A* search algorithm
 
@@ -136,6 +149,7 @@ def astar_search(start, goal, live_traffic, sunday_traffic):
         current = heapq.heappop(open_queue)
 
         if current.name == goal:
+            # If neighbor is not visited and not in the open_queue, add it to the queue
             path = []
             while current.name in came_from:
                 path.append((current.name, current.g_value))
@@ -146,7 +160,7 @@ def astar_search(start, goal, live_traffic, sunday_traffic):
 
         closed_set.add(current.name)
 
-        for neighbor, cost in live_traffic.get(current.name, []):
+        for neighbor, cost in sorted(live_traffic.get(current.name, [])):
             if neighbor in closed_set:
                 continue
 
@@ -154,7 +168,10 @@ def astar_search(start, goal, live_traffic, sunday_traffic):
             h_value = sunday_traffic[neighbor]
             neighbor_state = State(neighbor, g_value, h_value)
 
-            if neighbor_state in open_queue:
+            if any(
+                state.name == neighbor and state.g_value <= g_value
+                for state in open_queue
+            ):
                 continue
 
             heapq.heappush(open_queue, neighbor_state)
@@ -164,7 +181,7 @@ def astar_search(start, goal, live_traffic, sunday_traffic):
 
 
 def main():
-    # Read input.txt and perform the chosen search algorithm
+    # read input from input.txt file
     with open("input.txt", "r") as input_file:
         algorithm = input_file.readline().strip()
         start_state = input_file.readline().strip()
@@ -182,11 +199,8 @@ def main():
             time = int(time)
             sunday_traffic[state] = time
 
-    # Call the appropriate search algorithm function based on the specified algorithm
     if algorithm == "BFS":
         result = bfs_search(start_state, goal_state, live_traffic, sunday_traffic)
-        print("SHORTEST PATH:", result)
-
     elif algorithm == "DFS":
         result = dfs_search(start_state, goal_state, live_traffic, sunday_traffic)
     elif algorithm == "UCS":
@@ -197,7 +211,6 @@ def main():
         print("Invalid algorithm specified in input.txt")
         return
 
-    # Write the results to output.txt
     if result:
         with open("output.txt", "w") as output_file:
             for state, cost in result:
